430d13f0fec68edf336aa1fe20e0cc338067a997
sotzee <tianqi.zhao@stonybrook.edu>
2016-10-19 18:27:28 -0400

    remove later part of bad data due to weather. add removebad.py

---
+++ result/removebad.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 17 22:07:55 2016
+
+@author: Tianqi
+"""
+
+from astropy.io import ascii
+import numpy as np
+
+removelist=range(88)
+removelist=list(np.array(removelist)+320)
+for i in range(10):
+    data=ascii.read('guiding'+str(i+1)+'.cat')
+    data.remove_rows(removelist)
+    ascii.write(data, 'guiding'+str(i+1)+'.cat')
+
+data=ascii.read('target.cat')
+data.remove_rows(removelist)
+ascii.write(data,'target.cat')
