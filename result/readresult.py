d466a08c32a9bfe3b5f80208cbe57eb27af9d646
Ho Nam Nguyen <dialgavn@gmail.com>
2016-10-25 19:47:40 -0400

    normalize ratio with original data instead of binned data, output average and error for two stable regions

--- result/readresult.py
+++ result/readresult.py
@@ -28,9 +28,9 @@ def normalizeFlux(data,aper_num,omit_range):
             numer += data[i][flux]/(data[i][fluxerr]**2)
             denom += 1./(data[i][fluxerr]**2)
             count += 1
-    print data['NUMBER'][i]/60000.,' min'
+    #print data['NUMBER'][i]/60000.,' min'
     flux_ave = numer/denom
-    print "count = ",count
+    #print "count = ",count
     data[flux] = data[flux]/flux_ave
     data[fluxerr] = data[fluxerr]/flux_ave
     return data
@@ -43,13 +43,13 @@ data_guiding=list()
 for i in range(10):
     #ax = fig.add_subplot(3,4,i+1)
     data_guiding.append(normalizeFlux(ascii.read('guiding'+str(i+1)+'.cat'),2,[]))
-    print 'i = ',i
+    #print 'i = ',i
     #print data_guiding[i]['FLUX_APER_2']
     dummy = data_guiding[i]['FLUX_APER_2']
     for j in range(len(dummy)):
         if (dummy[j] > 1.03) or (dummy[j] < 0.97):
             data_guiding[i]['FLUXERR_APER_2'][j] = 1.e40
-            print data_guiding[i]['FLUX_APER_2'][j]
+            #print data_guiding[i]['FLUX_APER_2'][j]
 
     #ax.errorbar(data_guiding[i]['NUMBER'],data_guiding[i]['FLUX_APER_2'],yerr=data_guiding[i]['FLUXERR_APER_2'],fmt='o')
     #ax.set_ylim([0.95,1.05])
@@ -82,7 +82,49 @@ data_guiding_flux_master=data_guiding_flux_master/data_guiding_error_master
 
 transit_ratio=data_target['FLUX_APER_2']/data_guiding_flux_master
 
-### Bin it up - 5 minutes bins
+################ Let's find average of the two plateau################
+# 1st one: <45 mins and >155 mins
+# 2nd one: 75 mins < t < 125 mins
+r1 = 0.
+r2 = 0.
+numpt1 = 0
+numpt2 = 0
+for i in range(len(transit_ratio)):
+    time = data_target['NUMBER'][i]/60000.
+    if (time < 45.) or (time > 155.):
+        r1 += transit_ratio[i]
+        numpt1 += 1
+    elif (time > 75.) and (time < 125.):
+        r2 += transit_ratio[i]
+        numpt2 += 1
+r1 = r1/numpt1
+r2 = r2/numpt2
+print 'numpts: ',numpt1,numpt2
+
+#rerun loop to find errors
+rerr1 = 0.
+rerr2 = 0.
+numpt1 = 0
+numpt2 = 0
+for i in range(len(transit_ratio)):
+    time = data_target['NUMBER'][i]/60000.
+    if (time < 45.) or (time > 155.):
+        rerr1 += (transit_ratio[i]-r1)**2
+        numpt1 += 1
+    elif (time > 75.) and (time < 125.):
+        rerr2 += (transit_ratio[i]-r2)**2
+        numpt2 += 1
+rerr1 = np.sqrt(rerr1/numpt1/(numpt1-1.))
+rerr2 = np.sqrt(rerr2/numpt2/(numpt2-1.))
+
+print 'numpts: ',numpt1,numpt2
+print 'r1, rerr1: ',r1,rerr1
+print 'r2, rerr2: ',r2,rerr2
+print 'Flux ratio: ',r2/r1                                
+# Normalize?
+transit_ratio = transit_ratio/r1
+
+################## Bin it up - 5 minutes bins #####################
 binWidth = 5. #mins
 num_bin = int(np.ceil(max(data_target['NUMBER'])/60000./binWidth))
 bindata = np.zeros([num_bin,2])
@@ -92,17 +134,16 @@ j = 0
 for i in range(num_bin):
     num_point = 0
     while int(data_target['NUMBER'][j]/60000./binWidth) < (i+1):
-        print i,j,data_target['NUMBER'][j]/60000.
+        #print i,j,data_target['NUMBER'][j]/60000.
         bindata[i,1] += transit_ratio[j]
         bindata[i,0] += data_target['NUMBER'][j]/60000.
         num_point += 1
         j+=1
         if j == len(transit_ratio):
             break
-    print num_point
+    #print num_point
     bindata[i,1] = bindata[i,1]/num_point
     bindata[i,0] = bindata[i,0]/num_point
-
 
 # rerun loop to get errors
 j = 0
@@ -114,21 +155,23 @@ for i in range(num_bin):
         j+=1
         if j == len(transit_ratio):
             break
-    binerror[i] = np.sqrt(binerror[i]/num_point/(num_point-1))
-
-
+    binerror[i] = np.sqrt(binerror[i]/num_point/(num_point-1.))
+#####################################################################
+'''
 # Let's normalize one last time
 bindata_ave = 0.
 num_point = 0
 for i in range(len(bindata)):
     if (i<7) or (i>29):
-        print i
+        #print i
         bindata_ave += bindata[i,1]
         num_point += 1
 bindata_ave = bindata_ave/float(num_point)
 bindata[:,1] = bindata[:,1]/bindata_ave
+print bindata_ave
+'''
 
-#plt.plot(data_target['NUMBER'],transit_ratio,'o',label='wasp-2')
+#plt.plot(data_target['NUMBER']/60000.,transit_ratio,'o',label='wasp-2')
 #plt.plot(bindata[:,0],bindata[:,1],'o',label='wasp-2')
 plt.errorbar(bindata[:,0],bindata[:,1],yerr=binerror,fmt='o',label='wasp-2')
 #plt.plot(data_target['NUMBER'],data_target['FLUX_APER_2'],label='wasp-2')
